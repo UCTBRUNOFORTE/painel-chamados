a4bb89202223e6f0553ac6c5e583119e745da4b6
UCTBRUNOFORTE <87372806+UCTBRUNOFORTE@users.noreply.github.com>
2022-02-12 15:57:44 -0300

    Correção arquivo

---
+++ gti_pull.bat
@@ -0,0 +1,7 @@
+@echo off
+:x
+
+    git pull
+    TIMEOUT /T 10
+
+goto x
--- novoChamado.php
+++ novoChamado.php
@@ -1,4 +1,4 @@
 <?php
 
 
-file_put_contents(getcwd() . '/novos.txt', .   print_r($_REQUEST)  . date("F j, Y, g:i a") . , FILE_APPEND);
+file_put_contents(getcwd() . '/novos.txt',  print_r($_REQUEST), FILE_APPEND);
