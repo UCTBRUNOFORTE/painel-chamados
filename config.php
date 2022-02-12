5d8bbc601925c479f1c0f8e97de53fd70a8c731a
unknown <bruno.silva@uctechnology.com.br>
2022-02-12 14:25:18 -0300

    Adicionado conexao e configuração plataforma Tomticket

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('API_TOMTICKET','https://api.tomticket.com/chamados/60fb6d24a4174c4294e649f22b944a71/');
+
+define('API_BITRIX24','https://uctbrasil.bitrix24.com.br/rest/977/2wv7bdkfcd1ozw8r/crm.deal.list.json?');
+
+define('NOVOS_BITRIX24','FILTER[STAGE_ID]=C39:NEW');
+
+define('NOVOS_ATRASADOS_BITRIX24','FILTER[STAGE_ID]=C39:UC_LA4T73');
+
+define('EM_ATENDIMENTO_BITRIX24','FILTER[STAGE_ID]=C39:PREPARATION');
+
+define('ATRASADOS_BITRIX24','FILTER[STAGE_ID]=C39:PREPAYMENT_INVOIC');
+
+define('FINALIZADOS_BITRIX24','FILTER[STAGE_ID]=C39:WON');
+
---
+++ tomticket.php
@@ -0,0 +1,182 @@
+<div class="row">
+
+                        <!-- Chamados novos -->
+                        <div class="col-xl-4 col-md-6 mb-4">
+                            <table class="table table-borderless">
+                                <?php
+
+                                        $api = TRUE;
+
+                                        $contador = 1;
+
+                                        while($contador <= 2 ):
+
+                                    ?>
+
+                                <?php
+
+                                            
+                                            $response = @file_get_contents(API_TOMTICKET . $contador);
+
+                                            $chamados = json_decode($response, true); 
+
+                                            foreach ($chamados['data'] as $key => $chamado):  
+
+                                                if($chamado['ultimasituacao'] == 0):
+                                                                                        
+                                        ?>
+                                <tr>
+                                    <td>
+                                        <div class="card text-white bg-danger mb-3" style="mmax-width: 100%;">
+                                            <div class="card-header bg-danger"><?php echo $chamado['titulo']; ?></div>
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $chamado['categoria']; ?></h5>
+                                                <p class="card-text"><?php echo $chamado['mensagem']; ?></p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+
+
+
+                                <?php
+                                            endif;
+
+                                            endforeach;
+
+                                            $contador++;
+
+                                            endwhile;
+
+                                        ?>
+
+
+                            </table>
+                        </div>
+
+                        <!-- Chamados atribuidos -->
+                        <div class="col-xl-4 col-md-6 mb-4">
+                            <table class="table table-borderless" >
+                                <?php
+
+                                        $api = TRUE;
+
+                                        $contador = 1;
+
+                                        while($contador <= 2 ):
+
+                                    ?>
+
+                                <?php
+
+                                            
+                                            $response = @file_get_contents(API_TOMTICKET . $contador);
+
+                                            $chamados = json_decode($response, true); 
+
+                                            foreach ($chamados['data'] as $key => $chamado):  
+
+                                                if($chamado['ultimasituacao'] == 2 || $chamado['ultimasituacao'] == 3 || $chamado['ultimasituacao'] == 6):
+                                                                                        
+                                        ?>
+                                <tr>
+                                    <td>
+                                        <div class="card text-white bg-warning mb-3" style="mmax-width: 100%;">
+                                            <div class="card-header bg-warning"><?php echo $chamado['titulo']; ?></div>
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $chamado['categoria']; ?></h5>
+                                                <p class="card-text"><?php echo $chamado['mensagem']; ?></p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+
+
+
+                                <?php
+                                            endif;
+
+                                            endforeach;
+
+                                            $contador++;
+
+                                            endwhile;
+
+                                        ?>
+
+
+                            </table>
+                        </div>
+
+                        <!-- Chamados Finalizados -->
+                        <div class="col-xl-4 col-md-6 mb-4">
+                            <table class="table table-borderless">
+                                <?php
+
+                                        $api = TRUE;
+
+                                        $contador = 1;
+
+                                        while($contador <= 2 ):
+
+                                    ?>
+
+                                <?php
+
+                                            
+                                            $response = @file_get_contents(API_TOMTICKET . $contador);
+
+                                            $chamados = json_decode($response, true); 
+
+                                            foreach ($chamados['data'] as $key => $chamado):  
+
+                                                if($chamado['ultimasituacao'] == 5):
+                                                                                        
+                                        ?>
+                                <tr>
+                                    <td>
+                                        <div class="card text-white bg-success mb-3" style="mmax-width: 100%;">
+                                            <div class="card-header bg-success"><?php echo $chamado['titulo']; ?></div>
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $chamado['categoria']; ?></h5>
+                                                <p class="card-text">
+                                                    <?php 
+                                                    
+                                                        $palavras = explode(" ", $chamado['mensagem']);
+
+                                                        $descricao = "";
+
+                                                        for ($i = 0; $i <= 10; $i++) { 
+                                                            
+                                                            if(isset($palavras[$i])) $descricao .= " " . $palavras[$i];
+                                                            
+                                                        } 
+
+                                                        $descricao .= " ...";
+                                                            
+                                                        echo $descricao;
+                                                    ?>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+
+
+
+                                <?php
+                                            endif;
+
+                                            endforeach;
+
+                                            $contador++;
+
+                                            endwhile;
+
+                                        ?>
+
+
+                            </table>
+                        </div>
+
+                    </div>
